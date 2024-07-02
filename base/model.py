b61aa071cfb1e236a7458276961d2cc12f9a1968
akamikado <manishvasireddy1234@gmail.com>
2024-07-02 17:04:48 +0530

    fix: typo

--- base/model.py
+++ base/model.py
@@ -1,46 +1,5 @@
 import tensorflow as tf
-import numpy as np
-import math
-from pathlib import Path
-import matplotlig.pyplot as plt
-from utils.logging import LOGGER
-
-
-def feature_visualization(x, module_type, stage, n=32, save_dir=Path("runs/detect/exp")):
-    """
-    Visualize feature maps of a given model module during inference.
-
-    Args:
-        x (torch.Tensor): Features to be visualized.
-        module_type (str): Module type.
-        stage (int): Module stage within the model.
-        n (int, optional): Maximum number of feature maps to plot. Defaults to 32.
-        save_dir (Path, optional): Directory to save results. Defaults to Path('runs/detect/exp').
-    """
-    for m in {"Detect", "Segment", "Pose", "Classify", "OBB", "RTDETRDecoder"}:  # all model heads
-        if m in module_type:
-            return
-    if isinstance(x, tf.Tensor):
-        _, channels, height, width = x.shape  # batch, channels, height, width
-        if height > 1 and width > 1:
-            f = save_dir / \
-                f"stage{stage}_{module_type.split('.')[-1]}_features.png"  # filename
-
-            # select batch index 0, block by channels
-            blocks = tf.split(x[0], channels, axis=0)
-            n = min(n, channels)  # number of plots
-            _, ax = plt.subplots(math.ceil(n / 8), 8,
-                                 tight_layout=True)  # 8 rows x n/8 cols
-            ax = ax.ravel()
-            plt.subplots_adjust(wspace=0.05, hspace=0.05)
-            for i in range(n):
-                ax[i].imshow(blocks[i].squeeze())  # cmap='gray'
-                ax[i].axis("off")
-
-            LOGGER.info(f"Saving {f}... ({n}/{channels})")
-            plt.savefig(f, dpi=300, bbox_inches="tight")
-            plt.close()
-            np.save(str(f.with_suffix(".npy")), x[0].cpu().numpy())  # npy save
+from utils.plotting import feature_visualization
 
 
 def adaptive_avg_pool2d_squeeze(input_tensor):
@@ -67,7 +26,7 @@ class BaseModel(tf.keras.layers.Layer):
         if not hasattr(self, "criterion"):
             self.criterion = self.init_criterion()
 
-        prods = self.call(batch["img"]) if preds is None else preds
+        preds = self.call(batch["img"]) if preds is None else preds
         return self.criterion(preds, batch)
 
     def predict(self, x, profile=False, visualize=False, embed=None):
