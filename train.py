2ce231279a13bb400494c6bee2631af620344d66
akamikado <manishvasireddy1234@gmail.com>
2024-07-16 16:45:38 +0530

    fix: fixed weights file

--- train.py
+++ train.py
@@ -4,8 +4,8 @@ from model import ModifiedYolo
 # split = float(input("Enter the split: "))
 # split_dataset(split)
 
-model = ModifiedYolo(
-    "resnet-yolo.yaml").load("yolov8n-cls.pt")
+model = ModifiedYolo("yolov8n-cls.pt")
 
-results = model.train(data=f"isl_dataset-12",
+
+results = model.train(data="isl_dataset-12",
                       epochs=3, verbose=True, batch=1, optimizer='Adam', lr0=0.00050)
